bbb7493a90c69976e32a52af3562685728b63def
sanaz.jamloo@gmail.com <sanaz@Sanazs-MBP.attlocal.net>
2018-10-15 18:18:46 -0700

    Add body attributes to index.html

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_221049) do
+ActiveRecord::Schema.define(version: 2018_10_16_010123) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
   end
 
 end
